4018a42273c4846217c6f6e207fda10c96d4dc7c
MrDoritos <ianmoore322@gmail.com>
2018-12-20 13:56:47 -0500

    Uncommented code
    Some parts of the code didn't compile, uncommented.

--- conversions/itoa.h
+++ conversions/itoa.h
@@ -2,16 +2,16 @@
 #include <string.h>
 
 void itoa(int value, char* buf, int base){
-std::string s = /*std::to_string(value);*/"fix me conversions/itoa.h";
+std::string s = std::to_string(value);
 char const *pchar = s.c_str();
 for (int i = 0; i < s.length(); i++)
 buf[i] = pchar[i];
 }
 
 void itoa(int value, std::string &in){
-in = /*std::to_string(value);*/"fix me conversions/itoa.h";
+in = std::to_string(value);
 }
 
-//string* itoa(int value, std::string &in){
-//return &(in = std::to_string(value));
-//}
+string* itoa(int value, std::string &in){
+return &(in = std::to_string(value));
+}
