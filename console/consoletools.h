c08aa0fe4f3380c055370985de2f186fa273f17e
MrDoritos <ianmoore322@gmail.com>
2018-12-31 05:51:00 -0500

    um

--- console/consoletools.h
+++ console/consoletools.h
@@ -8,6 +8,8 @@
 #define IN_EXIT 1
 #define IN_OK 0
 
+//#define QUERYINDEX std::string("Enter number.")
+
 //bool consoletools::exiting;
 
 class consoletools {
@@ -24,6 +26,35 @@ std::cout << query << std::endl;
 std::getline(std::cin,out);
 if (exiting) return IN_EXIT; else return IN_OK;
 }
+int getmulti(std::string& query, std::string *options, int count, int& out, bool usedefault = false, int defaultvalue = 0) {
+if (usesignals) std::cout << "^C + ENTER to EXIT" << std::endl;
+std::cout << query << std::endl;
+if (usedefault) {
+std::cout << "Defaults to '" << options[defaultvalue] << "'" << std::endl;
+}
+for (int i = 0; i < count; i++)
+std::cout << "[" << i << "] " << options[i] << std::endl;
+
+int s;
+while (true) {
+out = 0;
+static std::string qq = std::string("Enter index value.");
+s = geti(qq, out, usedefault, defaultvalue);
+if (exiting) { if (usedefault) out = defaultvalue; return IN_EXIT; }
+switch (s) {
+case IN_OK:
+if (out >= count || out < 0) {
+std::cout << "Index out of bounds." << std::endl;
+break;
+} else {
+return IN_OK;
+}
+case IN_EXIT:
+if (usedefault) out = defaultvalue;
+return IN_EXIT;
+}
+}
+}
 int geti(std::string& query, int& out, bool usedefault = false, int defaultvalue = 0) {
 if (usesignals) std::cout << "^C + ENTER to EXIT" << std::endl;
 std::cout << query << std::endl;
@@ -37,6 +68,7 @@ out = 0;
 s = scanf("%i", &out);
 if (exiting) { if (usedefault) out = defaultvalue;  return IN_EXIT; }
 switch (s) {
+	//default:
 	case 1:
 		return IN_OK;
 	default:
