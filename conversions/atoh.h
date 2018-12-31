7d0c7d13508f80ff19a03ad7f571724aec35c802
MrDoritos <ianmoore322@gmail.com>
2018-12-31 17:45:12 -0500

    Really stupid mistake

--- conversions/atoh.h
+++ conversions/atoh.h
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #define __POS1(x) ((((x & 15) + 48) < 58) ? ((x & 15) + 48) : ((x & 15) + 65))
-#define __POS2(x) ((((x & 240) + 48) < 58) ? ((x & 240) + 48) : ((x & 240) + 65))
+#define __POS2(x) ((((x >> 4) + 48) < 58) ? ((x >> 4) + 48) : ((x >> 4) + 65))
 
 std::string* atoh(char* buffer, int size) {
 std::string* str = new std::string();
